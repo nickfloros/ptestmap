9aa76b128b8958aba92a8cb98cf71438f6b6a3ed
nickfloros <nickfloros@gmail.com>
2014-01-13 22:34:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic implementation of a polymer component google map component
---
+++ web/map_component.dart
@@ -0,0 +1,29 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+
+import 'package:google_maps/google_maps.dart';
+/**
+ * A Polymer click counter element.
+ */
+@CustomTag('map-component')
+class MapComponent extends PolymerElement {
+
+  Element _mapCanvas;
+  MapOptions _mapOptions;
+  GMap _map;
+  
+  MapComponent.created() : super.created() {
+    _mapOptions = new MapOptions()
+    ..zoom = 8
+    ..center = new LatLng(-34.397, 150.644)
+    ..mapTypeId = MapTypeId.ROADMAP
+    ;
+  }
+
+  void enteredView() {
+    super.enteredView();
+    _mapCanvas = $['map_canvas'];
+    _map = new GMap(_mapCanvas, _mapOptions);
+  }
+}
+
